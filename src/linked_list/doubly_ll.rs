0af5402dfedee941a6ee03dd2072da691a327fe3
hitmanbuzz <moirangthemhenthoiba30@gmail.com>
2025-03-30 22:05:47 +0530

    some extra methods added with dead_code allowed

--- src/linked_list/doubly_ll.rs
+++ src/linked_list/doubly_ll.rs
@@ -12,7 +12,8 @@ struct DoublyLinkedList<'a, T> {
     tail: Option<Rc<RefCell<Node<'a, T>>>>,
 }
 
-impl<'a, T: std::fmt::Debug + PartialEq + Copy + Clone> DoublyLinkedList<'a, T> {
+#[allow(dead_code)]
+impl<'a, T: std::fmt::Debug + PartialEq + Copy + Clone + std::fmt::Display> DoublyLinkedList<'a, T> {
     /// Create a new doubly linked list
     /// 
     /// Will create a new doubly linked list with no nodes
@@ -72,6 +73,70 @@ impl<'a, T: std::fmt::Debug + PartialEq + Copy + Clone> DoublyLinkedList<'a, T> 
             self.tail = Some(new_node);
         }
     }
+    
+    /// Insert a node/data after a certain node/data.
+    /// 
+    /// args:
+    /// * `_node`: The node required to push for `data`
+    ///
+    /// * `data`: The node/data to be inserted
+    fn push_after_node(&mut self, _node: &'a T, data: &'a T) {
+        if self.head.is_none() {
+            println!("List is empty");
+            return;
+        }
+
+        let mut current = self.head.clone();
+
+        while let Some(ref node) = current.clone() {
+            // println!("Node: {}", &node.borrow_mut().data);
+            if node.borrow_mut().data == _node {
+                let new_node = Rc::new(RefCell::new(Node {
+                    data,
+                    next: node.borrow().next.clone(),
+                    prev: node.borrow().prev.clone(), 
+                }));
+                
+                node.borrow_mut().next = Some(new_node);
+            }            
+            
+            current = node.borrow_mut().next.clone();
+        } 
+    }
+
+    /// Finding a node/data in a list and return the index of the node from the list
+    /// 
+    /// return: 
+    /// * `(bool, u32)` -> `(true, index)` | `(false, 0)`
+    fn find(&self, data: &'a T) -> (bool, u32) {
+        let mut current = self.head.clone();
+        let mut index_counter: u32 = 0;
+
+        while let Some(node) = current {
+            if node.borrow().data == data {
+                return (true, index_counter);
+            }
+            index_counter += 1;
+            current = node.borrow().next.clone();
+        } 
+        return (false, 0);
+    }
+    
+    /// Get the length (total no. of items in the list)
+    /// 
+    /// return: 
+    /// * `u32` -> The total number of items in the list
+    fn get_length(&self) -> u32 {
+        let mut current = self.head.clone();
+        let mut counter: u32 = 0;
+
+        while let Some(node) = current {
+            counter += 1;
+            current = node.borrow().next.clone();
+        }
+        
+        return counter;
+    }  
 
     /// Display the list
     fn display(&self) {
@@ -115,4 +180,14 @@ pub fn run() {
     doubly_ll.push_back(&80);
     doubly_ll.display();
     doubly_ll.get_tail_value();
+    
+    doubly_ll.push_after_node(&50, &969);
+    doubly_ll.display();
+    
+
+    doubly_ll.push_after_node(&969, &1000);
+    doubly_ll.display();
+    
+
+    println!("List Length: {}", doubly_ll.get_length());
 }
